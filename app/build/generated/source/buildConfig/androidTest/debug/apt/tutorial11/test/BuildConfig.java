ddee1936ddd4d6ecb05e0f1ebd567ed2d048a546
SangPham <anhsang222@gmail.com>
2017-05-12 13:56:34 +0700

    add tut11 2nd

--- app/build/generated/source/buildConfig/androidTest/debug/apt/tutorial11/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/apt/tutorial11/test/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package apt.tutorial.test;
+package apt.tutorial11.test;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "apt.tutorial.test";
+  public static final String APPLICATION_ID = "apt.tutorial11.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = -1;
